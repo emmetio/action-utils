7717ea5aff3be3018f0ac6c14d933bb40e455935
Sergey Chikuyonok <serge.che@gmail.com>
2019-10-30 00:14:30 +0300

    Keep dependencies external

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
     input: './src/index.ts',
+    external: ['emmet', '@emmetio/html-matcher', '@emmetio/css-matcher'],
     plugins: [nodeResolve(), typescript({
         tsconfigOverride: {
             compilerOptions: { module: 'esnext' }
